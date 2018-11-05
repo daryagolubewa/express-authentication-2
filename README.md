857c2e876de1bfd5d1017881646faab912aba570
Michael Klishevich <busation@gmail.com>
2018-11-05 18:38:08 +0300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # express-authentication
+
+## Learn
+
+1. Postman
+2. curl
+
+## Node modules
+
+1. uuid - generate session ids
+2. express-session - session support for express
+3. session-file-store - save session to file on server
+4. body-parser parse JSON and Form Data
+5. passport - express authentication middleware
+
+## Tasks
+
+1. Save sessions to database / file / variable
+2. Use bcrypt module for passwords - `npm install bcrypt-nodejs --save
+`
+3. Create HTML Sign In form 
+4. Create Sign Up routes and Sign Up form
--- server/server.js
+++ server/server.js
@@ -4,6 +4,48 @@ const uuid = require('uuid/v4');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const users = [
+    { id: '2f24vvg', email: 'test@test.com', password: 'password' }
+];
+
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    (email, password, done) => {
+        console.log('Inside local strategy callback');
+        // here is where you make a call to the database
+        // to find the user based on their username or email address
+        const foundUsers = users.filter(el => (el.email === email));
+        // no user was found
+        if(foundUsers.length === 0) {
+            return done('Error. Email not found!');
+        } else {
+            if(foundUsers[0].password === password) {
+                // success
+                console.log('Local strategy returned true');
+                return done(null, foundUsers[0]);
+            } else {
+                return done('Error. Password not correct!');
+            }
+        }
+    }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+    console.log('Inside serializeUser callback. User id is save to the session file store here');
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log('Inside deserializeUser callback');
+    console.log(`The user id passport saved in the session file store is: ${id}`);
+    const user = users[0].id === id ? users[0] : false;
+    done(null, user);
+});
 
 // create the server
 const app = express();
@@ -23,6 +65,8 @@ app.use(session({
     saveUninitialized: true,
     cookie: { maxAge: 10*60*1000 }
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // create the homepage route at '/'
 app.get('/', (req, res) => {
@@ -38,10 +82,38 @@ app.get('/login', (req, res) => {
     res.send(`You got the login page!\n`);
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', (req, res, next) => {
     console.log('Inside POST /login callback function');
-    console.log(req.body);
-    res.send(`You posted to the login page!\n`)
+    // console.log(req.body);
+    // res.send(`You posted to the login page!\n`)
+    passport.authenticate('local', (err, user, info) => {
+        console.log('err', err);
+        if (err) {
+            return res.send(err);
+        }
+        console.log('Inside passport.authenticate() callback');
+        console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+        console.log(`req.user: ${JSON.stringify(req.user)}`);
+        req.login(user, (err) => {
+            if (err) {
+                return res.send('req.login error', err);
+            }
+            console.log('Inside req.login() callback');
+            console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
+            console.log(`req.user: ${JSON.stringify(req.user)}`);
+            return res.send('You were authenticated & logged in!\n');
+        })
+    })(req, res, next);
+});
+
+app.get('/authrequired', (req, res) => {
+    console.log('Inside GET /authrequired callback');
+    console.log(`User authenticated? ${req.isAuthenticated()}`);
+    if(req.isAuthenticated()) {
+        res.send('you hit the authentication endpoint\n');
+    } else {
+        res.redirect('/');
+    }
 });
 
 // tell the server what port to listen on
